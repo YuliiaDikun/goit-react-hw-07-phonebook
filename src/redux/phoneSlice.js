623703d223671e7f8b04ff5e5fb8fadeab0ad864
Yuliia Dikun <107618913+YuliiaDikun@users.noreply.github.com>
2023-01-17 13:29:28 +0200

    refactor: extraReducers

--- src/redux/phoneSlice.js
+++ src/redux/phoneSlice.js
@@ -1,51 +1,47 @@
 import { createSlice } from '@reduxjs/toolkit';
-import { fetchTasks, addContact, deleteContact } from "./operations";
+import { fetchTasks, addContact, deleteContact } from './operations';
+
+const handlePending = state => {
+  state.isLoading = true;
+};
+const handleRejected = (state, action) => {
+  state.isLoading = false;
+  state.error = action.payload;
+};
+
 const phoneSlice = createSlice({
   name: 'phone',
-  initialState: { items: [], isLoading: false, error: null }, 
+  initialState: { items: [], isLoading: false, error: null },
+
   extraReducers: {
-    [fetchTasks.pending](state) {
-        state.isLoading = true;
-        state.error = null;
-    },
+    [fetchTasks.pending]: handlePending,
+    [addContact.pending]: handlePending,
+    [deleteContact.pending]: handlePending,
+
+    [fetchTasks.rejected]: handleRejected,
+    [addContact.rejected]: handleRejected,
+    [deleteContact.rejected]: handleRejected,
+
     [fetchTasks.fulfilled](state, action) {
-        state.isLoading = false;
-        state.error = null;
+      state.isLoading = false;
+      state.error = null;
       state.items = action.payload;
     },
-    [fetchTasks.rejected](state, action) {
-        state.isLoading = false;
-        state.error = action.payload;
+
+    [addContact.fulfilled](state, action) {
+      state.isLoading = false;
+      state.error = null;
+      state.items.push(action.payload);
     },
 
-    [addContact.pending](state) {
-        state.isLoading = true;
-      },
-      [addContact.fulfilled](state, action) {
-        state.isLoading = false;
-        state.error = null;
-        state.items.push(action.payload);
-      },
-      [addContact.rejected](state, action) {
-        state.isLoading = false;
-        state.error = action.payload;
-      },
-
-      [deleteContact.pending](state) {
-        state.isLoading = true;
-      },
-      [deleteContact.fulfilled](state, action) {
-        state.isLoading = false;
-        state.error = null;
-        const index = state.items.findIndex(
-          contact => contact.id === action.payload.id
-        );
-        state.items.splice(index, 1);
-      },
-      [deleteContact.rejected](state, action) {
-        state.isLoading = false;
-        state.error = action.payload;
-      },
+    [deleteContact.fulfilled](state, action) {
+      state.isLoading = false;
+      state.error = null;
+      const index = state.items.findIndex(
+        contact => contact.id === action.payload.id
+      );
+      state.items.splice(index, 1);
+    },
   },
 });
 
